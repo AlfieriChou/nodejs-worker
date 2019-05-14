29450acad561f2b059c362693e70f8891287bd5e
AlfieriChou <alfierichou@gmail.com>
2019-05-14 13:35:07 +0800

    rewrite message channel

--- messageChannel.js
+++ messageChannel.js
@@ -10,23 +10,37 @@ const { Worker, isMainThread, MessageChannel, parentPort } = require('worker_thr
 
 // testMC()
 
-if (isMainThread) {
-  const worker = new Worker(__filename)
-  const subChannel = new MessageChannel()
-  worker.postMessage({ hereIsYourPort: subChannel.port1 }, [subChannel.port1])
-  subChannel.port2.on('message', msg => {
-    console.log('received:', msg)
-  })
-} else {
-  parentPort.once('message', value => {
-    let testArr = []
-    for (let i = 0; i < 10; ++i) {
-      testArr[i] = 0
-      for (let j = i; j < 10; ++j) {
-        testArr[i] = i + j + testArr[i]
-      }
+const promiseSubChannel = () => {
+  return new Promise((resolve, reject) => {
+    if (isMainThread) {
+      const worker = new Worker(__filename)
+      const subChannel = new MessageChannel()
+      worker.postMessage({ hereIsYourPort: subChannel.port1 }, [subChannel.port1])
+      subChannel.port2.on('error', err => {
+        reject(err)
+      })
+      subChannel.port2.on('message', msg => {
+        resolve(msg)
+      })
+    } else {
+      parentPort.once('message', value => {
+        let testArr = []
+        for (let i = 0; i < 10; ++i) {
+          testArr[i] = 0
+          for (let j = i; j < 10; ++j) {
+            testArr[i] = i + j + testArr[i]
+          }
+        }
+        value.hereIsYourPort.postMessage(testArr)
+        value.hereIsYourPort.close()
+      })
     }
-    value.hereIsYourPort.postMessage(testArr)
-    value.hereIsYourPort.close()
   })
 }
+
+const bootstrap = async () => {
+  const result = await promiseSubChannel()
+  console.log('---->', result)
+}
+
+bootstrap()
